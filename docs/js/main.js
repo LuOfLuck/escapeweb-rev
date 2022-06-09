74cb5c439bf53d3d78c3f41d46489f7e673c0f66
Lu <you@example.com>
2022-06-08 23:40:07 -0300

    msg agregado

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,13 +1,45 @@
-const button = document.getElementById('banner__button__music');
+const buttonMusic = document.getElementById('banner__button__music');
 const background_sond = document.getElementById("background_sond");
-
-button.addEventListener("click", ()=>{
+const buttonAlert = document.getElementById('button__alert');
+const contAlert = document.getElementById('cont__alert');
+const textButton = buttonMusic.firstElementChild;
+buttonMusic.addEventListener("click", ()=>{
 	
-	if (/\ button__play\b/g.test(button.className)){
-		button.classList.replace("button__play", "button__pause");
+	if (/\ button__play\b/g.test(buttonMusic.className)){
+		buttonMusic.classList.replace("button__play", "button__pause");
 		background_sond.play()
+		textButton.innerText = "Pause";
+		//launchFullScreen(document.documentElement);
 	}else{
-		button.classList.replace("button__pause","button__play");
+		buttonMusic.classList.replace("button__pause","button__play");
 		background_sond.pause();
+		textButton.innerText = "Play";
 	}
 })
+
+buttonAlert.addEventListener("click", ()=>{
+	contAlert.classList.add("main__alert--active");
+	setTimeout(()=>{
+		contAlert.classList.remove("main__alert--active");
+	}, 5000);
+})
+
+
+function launchFullScreen(element) {
+  if(element.requestFullScreen) {
+    element.requestFullScreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullScreen) {
+    element.webkitRequestFullScreen();
+  }
+}
+ function cancelFullScreen() {
+     if(document.cancelFullScreen) {
+         document.cancelFullScreen();
+     } else if(document.mozCancelFullScreen) {
+         document.mozCancelFullScreen();
+     } else if(document.webkitCancelFullScreen) {
+         document.webkitCancelFullScreen();
+     }
+ }
