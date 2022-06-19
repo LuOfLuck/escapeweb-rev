f45f1d4defb15076d09fde41b73ad1deb46582de
Lu <you@example.com>
2022-06-19 00:51:04 -0300

    preperando vistas del index.html

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,59 +1,29 @@
-const buttonMusic = document.getElementById('banner__button__music');
-const background_sond = document.getElementById("background_sond");
-const textButton = buttonMusic.firstElementChild;
-const miStorage = window.localStorage;
 
-
-buttonMusic.addEventListener("click", ()=>{
-	
-	if (/\ button__play\b/g.test(buttonMusic.className)){
-		buttonMusic.classList.replace("button__play", "button__pause");
-		background_sond.play()
-		textButton.innerText = "Pause";
+const musicEfect = document.getElementById("music__efect");
+const musicSond = document.getElementById("music__sond");
+const backgroundSond = document.getElementById("background_sond");
+musicEfect.addEventListener("click", ()=>{
+	if (/\ music__element__efect--active\b/g.test(musicEfect.className)){
+		musicEfect.classList.remove("music__element__efect--active");
+		//background_sond.play()
 		//launchFullScreen(document.documentElement);
 	}else{
-		buttonMusic.classList.replace("button__pause","button__play");
-		background_sond.pause();
-		textButton.innerText = "Play";
+		musicEfect.classList.add("music__element__efect--active");
+		//background_sond.pause();
 	}
+
 })
-
-
-const parrafos = {
-	"banner":{
-		"eslogan":[
-			`Encerrado en la pantalla... <br>
-			Muy pronto :).`,
-			`Encerrado en la pantalla... <br>
-			Estamos trabajando en esto :).`,
-			`Encerrado en la pantalla... <br>
-			Vuelve pronto :).`,
-			`ey... ya basta por favor <br>
-			Espero que disfrutes el juego como ella disfruto jugar contigo :).`,
-			
-		],
-		"alert":[
-			`En este momento se esta desarrolladno Escape web, si buscas nuestra version anterior haz clic 
-			<a href='https://www.luofluck.tech/scapeweb/' target='_BLANK'>aqui</a>.`,
-			"ola",
-			"Op's ayudame a testear, ¿si?",
-			`¿ya no es seguiste en instagram?
-			<a href='https://www.instagram.com/EscapeWeb_rev/' target='_BLANK'>EscapeWeb_rev</a>.`,
-			`Buscar pistas no siempre es tan facil, a veces hay que hacer cosas que uno no espera`,
-		]	
+musicSond.addEventListener("click", ()=>{
+	if (/\ music__element__sond--active\b/g.test(musicSond.className)){
+		musicSond.classList.remove("music__element__sond--active");
+		//launchFullScreen(document.documentElement);
+		backgroundSond.pause();
+	}else{
+		musicSond.classList.add("music__element__sond--active");
+		backgroundSond.play()
 	}
 
-}
-/* ESLOGAN */
-if(miStorage["sesion"] == undefined){
-	localStorage.setItem('sesion', 0);
-}else{
-	miStorage["sesion"]++;
-}
-const eslogan = document.getElementById("eslogan");
-const lengthEslogan = parrafos.banner.eslogan.length;
-const msgEslogan = (lengthEslogan <= miStorage["sesion"]) ? miStorage["sesion"] = 0 : miStorage["sesion"];
-eslogan.innerHTML = parrafos.banner.eslogan[msgEslogan];
+})
 
 
 /* ALERTAS */
